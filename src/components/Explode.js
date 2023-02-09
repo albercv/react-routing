031f1541dbcb6a9c10e50072dd16b9ab016f7a1e
Alberto Carrasco <al.carrasco.vazquez@gmail.com>
2023-02-09 12:56:47 +0100

    First branch commit: Cleaning Bomb staff

--- src/components/Explode.js
+++ src/components/Explode.js
@@ -1,33 +1,32 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom'
-import {ErrorBoundary} from 'react-error-boundary'
+import { ErrorBoundary } from 'react-error-boundary'
 
-function ErrorFallback({error, resetErrorBoundary}) {
-    return (
-      <div role="alert">
-        <p>Something went wrong:</p>
-        <pre>{error.message}</pre>
-        <button onClick={resetErrorBoundary}>Try again</button>
-      </div>
-    )
-  }
-  
-  function Bomb() {
-    throw new Error('💥 CABOOM 💥')
-  }
-  
-  export function Explode() {
-    const [explode, setExplode] = React.useState(false)
-    return (
-      <div>
-        <button onClick={() => setExplode(e => !e)}>toggle explode</button>
-        <ErrorBoundary
-          FallbackComponent={ErrorFallback}
-          onReset={() => setExplode(false)}
-          resetKeys={[explode]}
-        >
-          {explode ? <Bomb /> : null}
-        </ErrorBoundary>
-      </div>
-    )
-  }
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
+function Bomb() {
+  throw new Error('💥 CABOOM 💥')
+}
+
+export function Explode() {
+  const [explode, setExplode] = React.useState(false)
+  return (
+    <div>
+      <button onClick={() => setExplode(e => !e)}>toggle explode</button>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => setExplode(false)}
+        resetKeys={[explode]}
+      >
+        {explode ? <Bomb /> : null}
+      </ErrorBoundary>
+    </div>
+  )
+}
